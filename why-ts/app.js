7e1bed456fa2807e2a703e413d06cb97afffbd83
ygvbhy <ygvbhy@naver.com>
2024-04-24 22:01:19 +0900

    간단한 js api 데이터 전송 후 화면에 출력

---
+++ why-ts/app.js
@@ -0,0 +1,28 @@
+// api url
+let url = "https://jsonplaceholder.typicode.com/users";
+
+// dom
+let username = document.querySelector("#username");
+let email = document.querySelector("#email");
+let address = document.querySelector("#address");
+
+// user data
+let user = {};
+
+function startApp() {
+  axios
+    .get(url)
+    .then(function (response) {
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표기하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
