43e67b00e1dbc84bee0b492348a5b573092486f8
ygvbhy <ygvbhy@naver.com>
2024-04-27 16:51:52 +0900

    기본 타입 - 튜플, 객체, 진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -11,3 +11,20 @@ let num: number = 10;
 let arr: Array<number> = [1, 2, 3];
 let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
 let items: number[] = [1, 2, 3];
+
+// TS 튜플 - 배열의 인덱스에 각각의 타입이 정해져 있다.
+let address: [string, number] = ["강남", 123];
+
+// TS 객체
+let obj: object = {};
+let persion: object = {
+  name: "capt",
+  age: 100,
+};
+let newPersion: { name: string; age: number } = {
+  name: "Thor",
+  age: 1000,
+};
+
+// TS 진위값
+let show: boolean = true;
