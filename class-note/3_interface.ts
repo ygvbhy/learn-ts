4999fd54fe64ebe42df897ae79aba1096584ed70
ygvbhy <ygvbhy@naver.com>
2024-05-02 21:57:56 +0900

    함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,20 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 let sego: User = {
   age: 19, 
   name: '세호'
 }
+
+// 함수의 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+
+const capt = {
+  name: '캡틴',
+  age: 100
+}
+
+getUser(capt)
