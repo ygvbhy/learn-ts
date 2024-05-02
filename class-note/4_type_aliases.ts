3f88f4a981784f5026fd436f546974d96f2eb9d5
ygvbhy <ygvbhy@naver.com>
2024-05-02 22:26:25 +0900

    타입 별칭과 인터페이스의 차이점

--- class-note/4_type_aliases.ts
+++ class-note/4_type_aliases.ts
@@ -1,3 +1,5 @@
+// https://joshua1988.github.io/ts/guide/type-alias.html#%ED%83%80%EC%9E%85-%EB%B3%84%EC%B9%AD%EC%9D%98-%ED%8A%B9%EC%A7%95
+
 // interface 로 정의 했을때는 interface의 명칭만 나옴
 // interface Person {
 //   name: string;
@@ -5,6 +7,7 @@
 // }
 
 // type 로 정의 했을때는 마우스 올리면 해당 타입에 대한 상세 정보까지 표기 됨
+// 단 type 은 확장이 불가능함. 그래서 interface 로 작성하는걸 추천함.
 type Person = {
   name: string;
   age: number;
