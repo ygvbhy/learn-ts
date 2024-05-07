e53b22cd5f517767ec281f47cb1dfc7e577d15c8
ygvbhy <ygvbhy@naver.com>
2024-05-07 21:44:10 +0900

    유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -6,7 +6,15 @@ logMessage("hello");
 
 // 유니온 타입 - Union Type
 // string 또는 number
+let sego: string | number | boolean;
 function logMessage1(value: string | number) {
-  console.log(value);
+  if (typeof value === "number") {
+    value.toLocaleString(); // 이렇게 들어올 경우 타입이 number로 추론 해서 들어옴
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError("value must be string or number");
 }
 logMessage1("hello");
+logMessage1(100);
