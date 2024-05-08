a78cdc4fefaa5ec73509a2e32eff3a8f6b9009f5
ygvbhy <ygvbhy@naver.com>
2024-05-08 22:09:59 +0900

    프로토타입과 클래스와의 관계

--- class-note/class.js
+++ class-note/class.js
@@ -1,3 +1,13 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+let capt = new Person("capt", 100);
+
+// 위의 코드와 아래의 코드는 완벽하게 동일한 코드임
+// 위의 코드가 ES2015 이전의 코드
+// 아래의 코드가 ES2015 이후의 코드
+
 class Person {
   // 클래스 로직
   constructor() {
