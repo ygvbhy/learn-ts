d77835de51eee23c946d1ecb35f837b798f0baee
ygvbhy <ygvbhy@naver.com>
2024-05-08 21:39:00 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,8 +1,21 @@
 // 이넘은 특정 값들의 집합을 의미하는 자료형.
+// 숫자형 이넘
+// Nike 에 10 이란 값을 입력 하면
+// Adidas 는 11이란 값이 생성 됨
+// 숫자형일 경우 1씩 증가 하는 집합
 enum Shoes {
-  Nike,
+  Nike = 10,
   Adidas,
 }
 
 let myShoes = Shoes.Nike;
-console.log(myShoes); // 0
+console.log(myShoes); // 10
+
+// 문자형 이넘
+enum stringShoes {
+  Nike = "나이키",
+  Adidas = "아디다스",
+}
+
+let stringMyShoes = stringShoes.Nike;
+console.log(stringMyShoes); // 나이키
