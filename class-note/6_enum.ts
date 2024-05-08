17f7b2f769a15628ed56d8bff462ebe8dc0ab76f
ygvbhy <ygvbhy@naver.com>
2024-05-08 21:45:07 +0900

    이넘 활용 사례

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -19,3 +19,38 @@ enum stringShoes {
 
 let stringMyShoes = stringShoes.Nike;
 console.log(stringMyShoes); // 나이키
+
+// 예제
+function askQuestion(answer: string) {
+  if (answer === "yes") {
+    console.log("정답입니다.");
+  }
+  if (answer === "no") {
+    console.log("오답입니다.");
+  }
+}
+askQuestion("yes");
+askQuestion("y");
+askQuestion("예스");
+askQuestion("Yes");
+// 위의 예제는 아래의 함수 호출 처럼 answer 의 값이 저런식으로 나뉠 수 있음
+
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+function askQuestion1(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log("정답입니다.");
+  }
+  if (answer === Answer.No) {
+    console.log("오답입니다.");
+  }
+}
+askQuestion1("yes");
+askQuestion1("y");
+askQuestion1("예스");
+askQuestion1("Yes");
+askQuestion1(Answer.Yes);
+// 이넘에서 제공되는 값만 넘길 수 있음
+// 드롭다운이나 어떤 목록 같은거에서 사용하면 이슈가 줄어들 거라 예상함
