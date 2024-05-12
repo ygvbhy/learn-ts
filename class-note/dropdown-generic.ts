7a67912b954ff9757cba3bd569cb6af88b62dcd6
ygvbhy <ygvbhy@naver.com>
2024-05-12 23:32:02 +0900

    예제 코드에 타입 선언 하기

--- class-note/dropdown-generic.ts
+++ class-note/dropdown-generic.ts
@@ -1,16 +1,25 @@
-const emails = [
+interface Email {
+  value: string;
+  selected: boolean;
+}
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
+
+const emails: Email[] = [
   { value: "naver.com", selected: true },
   { value: "google.com", selected: false },
   { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: ProductNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: Email | ProductNumber) {
   const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -21,5 +30,11 @@ function createDropdownItem(item) {
 emails.forEach(function (email) {
   const item = createDropdownItem(email);
   const selectTag = document.querySelector("#email-dropdown");
-  selectTag.appendChild(item);
+  selectTag?.appendChild(item);
+});
+
+numberOfProducts.forEach(function (number) {
+  const item = createDropdownItem(number);
+  const selectTag = document.querySelector("#product-dropdown");
+  selectTag?.appendChild(item);
 });
