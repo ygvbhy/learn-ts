d1dae241884b8803807780e79e7d0aa0c652baa2
ygvbhy <ygvbhy@naver.com>
2024-05-12 17:04:20 +0900

    기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언의 단점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -12,3 +12,14 @@ function text<T>(text: T): T {
   return text;
 }
 text<string>("hi");
+
+function log(text: string) {
+  console.log(text);
+  return text;
+}
+
+function logNumber(num: number) {
+  console.log(num);
+  return num;
+}
+// 위의 두개의 함수는 같은 로직이지만 타입별로 따로 선언 되어 있기 떄문에 유지보수 할때 힘듬
