70e4732a2a1a71d6cf1dc4a1f3fe01440185d25d
ygvbhy <ygvbhy@naver.com>
2024-05-12 17:08:19 +0900

    유니온 타입을 이용한 선언 방식의 문제점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -23,3 +23,12 @@ function logNumber(num: number) {
   return num;
 }
 // 위의 두개의 함수는 같은 로직이지만 타입별로 따로 선언 되어 있기 떄문에 유지보수 할때 힘듬
+
+function logText1(text: string | number) {
+  console.log(text);
+  return text;
+}
+
+const a = logText1("a");
+// 유니온 타입으로 문자열과 숫자의 타입을 받을 수 있어졌으나 반환값에 대한 타입 설정이 잘못되어 있으므로 문제가 생김
+logText1(10);
