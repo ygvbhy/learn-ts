aa6070adce89061e5bdf3fe4efdd5a953a8e9c01
ygvbhy <ygvbhy@naver.com>
2024-05-14 22:38:46 +0900

    타입 추론 기본 3

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -29,3 +29,22 @@ const User: Test<number | string> = {
   value: 10,
   name: "Cap",
 };
+
+// 타입 추론 기본 3
+interface Dropdown1<T> {
+  value: T;
+  title: string;
+}
+// Dropdown1 의 타입을 상속 받아서 DetailedDripdown 로 들어온 제네릭 K 값이 Dropdown1 의 제네릭 T가 K 값으로 들어감
+interface DetailedDripdown<K> extends Dropdown1<K> {
+  description: string;
+  tag: K;
+}
+
+// string 을 넣어서 위의 K, T 인 제네릭 값은 string 으로 설정 됨
+let detailedItem: DetailedDripdown<string> = {
+  title: "abc",
+  description: "ab",
+  value: "a",
+  tag: "a",
+};
