44d85d02ee4a9c9b277c967c4d4296e0059b56c2
ygvbhy <ygvbhy@naver.com>
2024-05-14 22:33:26 +0900

    타입 추론 기본 2

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,3 +1,4 @@
+// 타입 추론 기본 1
 // 기본적으로 any 로 설정 됨
 let a;
 
@@ -7,3 +8,24 @@ function getB(b = 10) {
   let c = "hi";
   return b + c;
 }
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+let shoppingItem: Dropdown<string> = {
+  value: "abc",
+  title: "hello",
+};
+
+// 궁금 한거 테스트 용
+interface Test<T> {
+  value: T;
+  name: T;
+}
+
+const User: Test<number | string> = {
+  value: 10,
+  name: "Cap",
+};
