cafeba5c6eb4973442c0b87ff86c5070bb018229
ygvbhy <ygvbhy@naver.com>
2024-07-16 21:33:47 +0900

    유틸리티 타입 상세 설명

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -39,3 +39,35 @@ type updateProduct1 = Partial<Product>
 function updateProductItem(productItem: UpdateProduct) {
 
 }
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+// interface IserProfileUpdate {
+//   username?: string;
+//   email?: string;
+//   profilePhotoUrl?: string;
+// }
+// #1
+type UserProfileUpdate = {
+  username?: UserProfile['username']
+  email?: UserProfile['email']
+  profilePhotoUrl?: UserProfile['profilePhotoUrl']
+}
+// #2
+type UserProfileUpdate2 = {
+  [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]
+}
+// #3
+type UserProfileUpdate3 = {
+  [p in keyof UserProfile]?: UserProfile[p]
+}
+// #4
+type Subset<T> = {
+  [p in keyof T]?: T[p]
+}
+type UserProfileUpdate4 = Partial<UserProfile>
+
